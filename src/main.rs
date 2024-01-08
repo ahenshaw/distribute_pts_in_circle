3fdd1dfe9f2a9f414178c09f67d585b3876380f6
Andrew Henshaw <andrew.henshaw@gtri.gatech.edu>
2024-01-08 10:52:08 -0500

    switched to clap for subcommand support

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,81 @@
-use argh::FromArgs;
+use clap::{Parser, Subcommand};
+use printpdf::{Mm, PdfDocument, Point as PdfPoint, Polygon, PolygonMode, WindingOrder};
 use std::f64::consts::PI;
+use std::fs::File;
+use std::io::BufWriter;
 use std::path::{Path, PathBuf};
 use svg::node::element::Circle;
 use svg::Document;
-
 type Point = (f64, f64);
 type Points = Vec<Point>;
 
-#[derive(FromArgs)]
 /// Distribute points in a circle
+#[derive(Parser)]
+#[clap(name = "distribute-pts-in-circle", version)]
 struct Args {
     /// number of points
-    #[argh(positional)]
     n: usize,
-    /// output file path
-    #[argh(positional)]
-    output: PathBuf,
     /// boundary evenness
-    #[argh(option, short = 'a', default = "0.0")]
+    #[clap(short = 'a', long, default_value_t = 0.0)]
     alpha: f64,
     /// dot size
-    #[argh(option, short = 'd', default = "5.0")]
+    #[clap(short = 'd', long, default_value_t = 5.0)]
     dotsize: f64,
+    #[clap(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Svg {
+        /// SVG file path
+        output: PathBuf,
+    },
+    Pdf {
+        /// PDF file path
+        output: PathBuf,
+    },
 }
 
 fn main() {
-    let args: Args = argh::from_env();
+    let args = Args::parse();
     let pts = sunflower(args.n, args.alpha);
-    write_to_svg(&args.output, &pts, args.dotsize);
+    match &args.commands {
+        Commands::Svg { output } => write_to_svg(&output, &pts, args.dotsize),
+        Commands::Pdf { output } => write_to_pdf(&output, &pts, args.dotsize),
+    }
+}
+
+fn write_to_pdf(outpath: &Path, pts: &Points, _dotsize: f64) {
+    let (doc, page1, layer1) = PdfDocument::new(
+        "Points in a circle".to_string(),
+        Mm(297.0),
+        Mm(210.0),
+        "Layer 1",
+    );
+    let current_layer = doc.get_page(page1).get_layer(layer1);
+
+    // Quadratic shape. The "false" determines if the next (following)
+    // point is a bezier handle (for curves)
+    // If you want holes, simply reorder the winding of the points to be
+    // counterclockwise instead of clockwise.
+    let points1 = vec![
+        (PdfPoint::new(Mm(100.0), Mm(100.0)), false),
+        (PdfPoint::new(Mm(100.0), Mm(200.0)), false),
+        (PdfPoint::new(Mm(300.0), Mm(200.0)), false),
+        (PdfPoint::new(Mm(300.0), Mm(100.0)), false),
+    ];
+
+    // Is the shape stroked? Is the shape closed? Is the shape filled?
+    let line1 = Polygon {
+        rings: vec![points1],
+        mode: PolygonMode::Stroke,
+        winding_order: WindingOrder::NonZero,
+    };
+    current_layer.add_polygon(line1);
+    doc.save(&mut BufWriter::new(File::create(outpath).unwrap()))
+        .unwrap();
+    println!("Created PDF ({:?}) with {} points", outpath, pts.len());
 }
 
 fn write_to_svg(outpath: &Path, pts: &Points, dotsize: f64) {
@@ -48,6 +96,7 @@ fn write_to_svg(outpath: &Path, pts: &Points, dotsize: f64) {
             .set("stroke-width", "2"),
     );
     svg::save(outpath, &document).unwrap();
+    println!("Created SVG ({:?}) with {} points", outpath, pts.len());
 }
 
 fn sunflower(n: usize, alpha: f64) -> Points {
