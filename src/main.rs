40767f8f36390090d1597af16a2a5b18bed4dc8f
Andrew Henshaw <andrew.henshaw@gtri.gatech.edu>
2024-01-08 12:55:03 -0500

    added PDF output for exact size drawings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
-use printpdf::{Mm, PdfDocument, Point as PdfPoint, Polygon, PolygonMode, WindingOrder};
+use printpdf::{
+    calculate_points_for_circle, Color, Mm, PdfDocument, Polygon, PolygonMode, Rgb, WindingOrder,
+};
 use std::f64::consts::PI;
 use std::fs::File;
 use std::io::BufWriter;
@@ -18,9 +20,6 @@ struct Args {
     /// boundary evenness
     #[clap(short = 'a', long, default_value_t = 0.0)]
     alpha: f64,
-    /// dot size
-    #[clap(short = 'd', long, default_value_t = 5.0)]
-    dotsize: f64,
     #[clap(subcommand)]
     commands: Commands,
 }
@@ -30,10 +29,19 @@ enum Commands {
     Svg {
         /// SVG file path
         output: PathBuf,
+        /// dot size
+        #[clap(short = 'd', long, default_value_t = 5.0)]
+        dotsize: f64,
     },
     Pdf {
         /// PDF file path
         output: PathBuf,
+        /// dot size (in mm)
+        #[clap(short = 'd', long, default_value_t = 0.5)]
+        dotsize: f32,
+        /// circle radius (in mm)
+        #[clap(short = 'r', long, default_value_t = 75.0)]
+        radius: f32,
     },
 }
 
@@ -41,38 +49,54 @@ fn main() {
     let args = Args::parse();
     let pts = sunflower(args.n, args.alpha);
     match &args.commands {
-        Commands::Svg { output } => write_to_svg(&output, &pts, args.dotsize),
-        Commands::Pdf { output } => write_to_pdf(&output, &pts, args.dotsize),
+        Commands::Svg { output, dotsize } => write_to_svg(&output, &pts, *dotsize),
+        Commands::Pdf {
+            output,
+            radius,
+            dotsize,
+        } => write_to_pdf(&output, &pts, *radius, *dotsize),
     }
 }
 
-fn write_to_pdf(outpath: &Path, pts: &Points, _dotsize: f64) {
+fn pdf_circle(x: f32, y: f32, r: f32, mode: PolygonMode) -> Polygon {
+    let circle = Polygon {
+        rings: vec![calculate_points_for_circle(Mm(r), Mm(x), Mm(y))],
+        mode,
+        winding_order: WindingOrder::NonZero,
+    };
+    circle
+}
+fn write_to_pdf(outpath: &Path, pts: &Points, radius: f32, dotsize: f32) {
+    let margin = 10.0;
     let (doc, page1, layer1) = PdfDocument::new(
         "Points in a circle".to_string(),
-        Mm(297.0),
-        Mm(210.0),
+        Mm((radius + dotsize + margin) * 2.0),
+        Mm((radius + dotsize + margin) * 2.0),
         "Layer 1",
     );
     let current_layer = doc.get_page(page1).get_layer(layer1);
+    current_layer.set_outline_thickness(0.05);
+    current_layer.set_outline_color(Color::Rgb(Rgb::new(0.75, 0.75, 0.75, None)));
+    let circle = pdf_circle(
+        radius + dotsize + margin,
+        radius + dotsize + margin,
+        radius,
+        PolygonMode::Stroke,
+    );
+    current_layer.add_polygon(circle);
 
-    // Quadratic shape. The "false" determines if the next (following)
-    // point is a bezier handle (for curves)
-    // If you want holes, simply reorder the winding of the points to be
-    // counterclockwise instead of clockwise.
-    let points1 = vec![
-        (PdfPoint::new(Mm(100.0), Mm(100.0)), false),
-        (PdfPoint::new(Mm(100.0), Mm(200.0)), false),
-        (PdfPoint::new(Mm(300.0), Mm(200.0)), false),
-        (PdfPoint::new(Mm(300.0), Mm(100.0)), false),
-    ];
+    // current_layer.set_outline_color(Color::Rgb(Rgb::new(0.0, 0.0, 0.0, None)));
+    let offset = radius + dotsize + margin;
+    for (x, y) in pts {
+        let dot = pdf_circle(
+            (*x as f32) * radius + offset,
+            (*y as f32) * radius + offset,
+            dotsize,
+            PolygonMode::Fill,
+        );
+        current_layer.add_polygon(dot);
+    }
 
-    // Is the shape stroked? Is the shape closed? Is the shape filled?
-    let line1 = Polygon {
-        rings: vec![points1],
-        mode: PolygonMode::Stroke,
-        winding_order: WindingOrder::NonZero,
-    };
-    current_layer.add_polygon(line1);
     doc.save(&mut BufWriter::new(File::create(outpath).unwrap()))
         .unwrap();
     println!("Created PDF ({:?}) with {} points", outpath, pts.len());
@@ -105,14 +129,14 @@ fn sunflower(n: usize, alpha: f64) -> Points {
     let b = (alpha * (n as f64).sqrt()).round() as usize;
     (1..=n)
         .map(|k| {
-            let r = radius(k, n, b);
+            let r = calc_radius(k, n, b);
             let theta = angle_stride * (k as f64);
             (r * theta.cos(), r * theta.sin())
         })
         .collect()
 }
 
-fn radius(k: usize, n: usize, b: usize) -> f64 {
+fn calc_radius(k: usize, n: usize, b: usize) -> f64 {
     if k > n - b {
         1.0
     } else {
